021af31f91aeff9433332fd77bf5dfdeb83d21cd
Yaxyobek03 <yaxyobekbahromjonov44@gmail.com>
2023-10-23 17:37:07 +0500

    added header logo

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,14 +1,18 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 
+// Imgs
+import logo from '../assets/Logo.svg'
 const Header = () => {
     return (
-        <div className='bg-HappyYipee '>
-           <div className='main-container'>
-            <Link className='font-releway'>Header</Link>
-            
+        <div className='bg-HappyYipee py-7'>
+            <div className='main-container '>
+                <Link className=''>
+                    <img className='w-36 h-6 ' src={logo} alt="logo img" />
+                </Link>
 
-           </div>
+
+            </div>
 
         </div>
     )
